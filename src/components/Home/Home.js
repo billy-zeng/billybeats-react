ef9d7346a96ad81b3ad4428b26375ce0aa80965f
billy-zeng <bzeng95@gmail.com>
2020-05-04 02:55:21 -0700

    adds base home page and footer component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+const Home = props => {
+  return (
+    <div className="landing text-light pt-5">
+      <h1>Welcome to BillyBeats</h1>
+    </div>
+  );
+}
+
+export default Home;
