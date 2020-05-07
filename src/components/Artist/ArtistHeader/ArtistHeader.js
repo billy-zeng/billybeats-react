8bf5d3f12a65d7c5e4434b2e8b4ada4ebb55d363
billy-zeng <bzeng95@gmail.com>
2020-05-06 23:34:22 -0700

    adds artist image

--- src/components/Artist/ArtistHeader/ArtistHeader.js
+++ src/components/Artist/ArtistHeader/ArtistHeader.js
@@ -60,7 +60,7 @@ class ArtistHeader extends React.Component {
     return(
       <Container id="artist-header" className="d-flex flex-row align-items-start justify-content-center mt-3">
         <div id="artist-img-wrapper" className="d-flex col-sm-3 align-items-center justify-content-center">
-          <img id="artist-picture" src="https://iupac.org/wp-content/uploads/2018/05/default-avatar.png" alt="profile-pic"/>
+          <img id="artist-picture" src={this.props.artistData.image} alt="profile-pic"/>
         </div>
         <div id="artist-overview" className="col-sm-9 d-flex flex-column align-items-start justify-content-center text-light bg-dark pl-5">
           <h1>{this.props.artistData.name}</h1>
